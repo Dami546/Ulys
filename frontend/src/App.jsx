9b3e80bbff33395ca5bde89555be0affcf379ea7
Dami546 <damikaif@gmail.com>
2026-03-07 20:10:27 +0300

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ 
+      backgroundColor: '#FF2D3E', // Красный из твоего макета
+      height: '100vh', 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center',
+      margin: 0
+    }}>
+      <h1 style={{ color: 'white', fontSize: '64px', fontWeight: 'bold', fontFamily: 'sans-serif' }}>
+        Ulys
+      </h1>
+    </div>
+  );
+}
+
+export default App;
