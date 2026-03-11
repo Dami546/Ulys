1c77cbee222dfe4dbf2ea14af8863876dd3b2c28
Dami546 <damikaif@gmail.com>
2026-03-11 23:18:10 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+/**
+ * Это входная точка вашего React-приложения.
+ * ReactDOM берет ваш основной компонент <App /> и вставляет его 
+ * в HTML-элемент с id="root", который находится в public/index.html.
+ */
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+
+  root.render(
+    <React.StrictMode>
+      {/* Весь ваш интерфейс и логика теперь подгружаются отсюда */}
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Не удалось найти корневой элемент. Проверьте наличие <div id='root'></div> в вашем index.html");
+}
